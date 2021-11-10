c710b495eccbf2c8f59010f1b88df92a3e3a42ee
SajdaAlava <sajdaalawad@gmail.com>
2021-11-11 02:19:13 +0300

    first commit

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,25 @@
+
+@extends('layouts.home')
+
+@section('title', 'Sub Page Title')
+
+@section('sidebar')
+    <div class="col-sm-4 col-lg-2" style="background-color:#2563eb">
+       <ul>
+           <li>Bilgisayar</li>
+           <li>Elektronik</li>
+           <li>Ev Esyalari</li>
+       </ul>
+    </div>
+@endsection
+
+@section('content')
+  <div class="col-sm-8 col-lg-10">
+   <h1>Slider</h1>
+    zzzzzzzzzz <br>
+    <h1> Iceriklerimiz</h1>
+    dddddddddddddd <br>
+    fffffffffffffff <br>
+
+  </div>
+@endsection
