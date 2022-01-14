e3bf2f0a058004b16cf4ab9dc502feb84680cfe1
SajdaAlava <sajdaalawad@gmail.com>
2022-01-15 01:52:54 +0300

    Airport page addet to admin

--- app/Models/Airport.php
+++ app/Models/Airport.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Airport extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
